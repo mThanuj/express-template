4b2c38740bb943e614986f043bc9bf2353369945
mThanuj <mullagurithanuj0@gmail.com>
2026-01-31 14:53:53 +0530

    fixed

--- vitest.config.js
+++ vitest.config.js
@@ -1,3 +1,13 @@
 import { defineConfig } from "vitest/config";
+import { fileURLToPath, URL } from "node:url";
 
-export default defineConfig({});
+export default defineConfig({
+  test: {
+    globals: true,
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
